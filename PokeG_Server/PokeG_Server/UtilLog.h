e4a43c5095731e8dfb7ade78ee389f14997d1b8d
Baek Jong Hwa <somang00915@tukorea.ac.kr>
2025-05-06 23:07:25 +0900

    Log 출력 제작 중

---
+++ PokeG_Server/PokeG_Server/UtilLog.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <format>
+
+class UtilLog
+{
+public:
+	template<typename Args>
+	static void Log(const Args message);
+
+	template<typename... Args>
+	static void Log(std::format_string<Args...> fmt, Args&&... args);
+};
